b83fa9fb6c3ebfc6e761e3abbc0afd9e782e5b8b
Itoi <Itoiandre14@gmail.com>
2025-10-20 17:34:32 -0300

    criando usuarios

---
+++ codigos/main.py
@@ -0,0 +1,4 @@
+from utils import *
+a = input(), input(), input()
+usuarios = [criar_usuario(*a)]
+print(usuarios)
---
+++ codigos/utils.py
@@ -0,0 +1,4 @@
+#serve para criar as funções auxiliares
+
+def criar_usuario(nome, email, perfil):
+    return {"nome": nome, "email": email, "perfil": perfil}
