15f71947d67a4201aff3734472e43f8bf10b7d31
Itoi <Itoiandre14@gmail.com>
2025-10-21 16:04:29 -0300

    ta horrivel mas cansei

--- codigos/main.py
+++ codigos/main.py
@@ -3,7 +3,7 @@ import time
 import os
 lista = []
 while 1:
-    #os.system("cls")
+    os.system("cls")
     inicio()
     opcao = input()
     if opcao == "1":
@@ -13,25 +13,46 @@ while 1:
         if opcao_nsei == "0":
             continue
         elif opcao_nsei == "1":
-            
             nome = input("nome: ")
             if nome == "":
                 print("nome não deve ser vazio!")
+                time.sleep(1)
+                continue
             email = input("email: ")
             #Criar uma verificação do email se já existe!
             perfil = input("perfil: ")
             lista.append(cadastrar_usuario(nome, email, perfil))
+            print("Contato adicionado!")
+            time.sleep(1)
         elif opcao_nsei == "2":
             if lista == []:
                 print("Nenhum usuario adicionado!")
             else:
                 listar_usuarios(lista)
                 time.sleep(3)
-            
+        elif opcao_nsei == "3":
+            nome = input("Digite o nome: ")
+            for dicionario in lista:
+                if nome == dicionario["nome"]:
+                    print(f"nome: {dicionario["nome"]}")
+                    print(f"email: {dicionario["email"]}")
+                    print(f"perfil: {dicionario["perfil"]}")
+                    time.sleep(3)
+                    continue
+                else:
+                    email = input("email: ")
+                    for dicionario in lista:
+                        if email == dicionario["email"]:
+                            print(f"nome: {dicionario["nome"]}")
+                            print(f"email: {dicionario["email"]}")
+                            print(f"perfil: {dicionario["perfil"]}")
+                            time.sleep(3)
+                            continue
     elif opcao == "2":
         print("Você escolheu Projetos")
     elif opcao == "3":
         print("Você escolheu Tarefas")
+    
     else:
         print("Opção inválida!")
         time.sleep(1)
