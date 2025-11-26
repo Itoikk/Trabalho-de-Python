f0ffa2315f0e0e64b8346be1d99008c11e39b7ce
Bartholomeulcs <bartholomeu.l@aluno.ifsp.edu.br>
2025-11-26 17:40:26 -0300

    aa

--- README.md
+++
@@ -1,2 +0,0 @@
-# Trabalho-de-Python
-agora foi?
--- codigos/models.py
+++
@@ -1,5 +0,0 @@
-def cadastrar_usuario(nome, email, perfil):
-    return {"nome": nome,
-            "email": email,
-            "perfil": perfil
-               }
